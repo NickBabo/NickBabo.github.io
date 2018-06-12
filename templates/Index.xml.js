2340cd3e332792045b4a5fa0a790822eea6edbd2
Nicholas Babo <nickbabo7@gmail.com>
2018-06-12 19:38:02 -0300

    mudando template

--- templates/Index.xml.js
+++ templates/Index.xml.js
@@ -1,27 +1,32 @@
 /*
 	Copyright (C) 2015 Apple Inc. All Rights Reserved.
 	See LICENSE.txt for this sample’s licensing information
-	
+
 	Abstract:
 	A list template shows a list of items on the right, such as movies or TV shows. Focus on one to see its related content on the left, such as its artwork or description.
 */
 var Template = function() {
 	return '<?xml version="1.0" encoding="UTF-8"?>' +
-	'<document>' +
-		'<listTemplate>' +
-			'<list>' +
-				'<header>' +
-					'<title>Playback examples</title>' +
-				'</header>' +
-				'<section>' +
-					'<listItemLockup id="video">' +
-						'<title>Video</title>' +
-					'</listItemLockup>' +
-					'<listItemLockup id="playlist">' +
-						'<title>Playlist</title>' +
-					'</listItemLockup>' +
-				'</section>' +
-			'</list>' +
-		'</listTemplate>' +
-	'</document>';
+  '<document>' +
+  '<stackTemplate>' +
+  '<banner>' +
+  '<title>Available Action Movies</title>'+
+  '</banner> '+
+  '<collectionList> '+
+  '<shelf> '+
+  '<section> '+
+  '<lockup onselect="playMedia(\'https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4\', \'video\')">' +
+  '<img src="path to image" width="182" height="274"/> '+
+  '<title>Movie 1</title>' +
+  '</lockup> '+
+  '<lockup onselect="playMedia('path to video', 'video')"> '+
+  '<img src="path to image" width="182" height="274" /> '+
+  '<title>Movie 2</title> '+
+  '</lockup>' +
+  '</section>' +
+  '</shelf> '+
+  '</collectionList> '+
+  '</stackTemplate>' +
+  '</document>' +
+
 }
